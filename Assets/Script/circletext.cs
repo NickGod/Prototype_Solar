5e1966de979a9d243dcf24f5fb0a55ad11a709b1
lunaticAnn <lunatic_ann@hotmail.com>
2017-02-09 15:52:51 -0500

    WT*??700+

---
+++ Assets/Script/circletext.cs
@@ -0,0 +1,62 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+using System.Collections.Generic;
+
+public class circletext : BaseMeshEffect {
+
+  
+    public int radius = 50;
+    public float spaceCoff = 1f;
+
+    Text _myText;
+
+    #region implemented absract members
+    public override void ModifyMesh(VertexHelper vh) {
+        //text component
+        _myText = GetComponent<Text>();
+        TextGenerator _tg = _myText.cachedTextGenerator;
+        List<UIVertex> stream = new List<UIVertex>();
+        vh.GetUIVertexStream(stream);
+
+        float parameter = Mathf.PI * radius * 2;
+        float weight = _myText.fontSize / parameter * spaceCoff;
+        float radStep = Mathf.PI * 2 * weight;
+        float charOffset = _tg.characterCountVisible / 2f - 0.5f;
+
+        for (int i = 0; i < _tg.characterCountVisible; i++)
+        {
+
+            var lt = stream[i * 6];
+            var rt = stream[i * 6 + 1];
+            var rb = stream[i * 6 + 2];
+            var lb = stream[i * 6 + 4];
+
+            Vector3 center = Vector3.Lerp(lb.position, rt.position, 0.5f);
+            Matrix4x4 move = Matrix4x4.TRS(center * -1, Quaternion.identity, Vector3.one);
+            float rad = Mathf.PI / 2 + (charOffset - i) * radStep;
+            Vector3 pos = new Vector3(Mathf.Cos(rad), Mathf.Sin(rad), 0) * radius;
+
+            Quaternion rotation = Quaternion.Euler(0, 0, rad * 180 / Mathf.PI - 90);
+            Matrix4x4 rotate = Matrix4x4.TRS(Vector3.zero, rotation, Vector3.one);
+            Matrix4x4 place = Matrix4x4.TRS(pos, Quaternion.identity, Vector3.one);
+            Matrix4x4 transform = place * rotate * move;
+
+            lb.position = transform.MultiplyPoint(lb.position);
+            lt.position = transform.MultiplyPoint(lt.position);
+            rt.position = transform.MultiplyPoint(rt.position);
+            rb.position = transform.MultiplyPoint(rb.position);
+
+            stream[i * 6] = lt;
+            stream[i * 6 + 1] = rt;
+            stream[i * 6 + 2] = rb;
+            stream[i * 6 + 3] = rb;
+            stream[i * 6 + 4] = lb;
+            stream[i * 6 + 5] = lt;
+        }
+
+        vh.Clear();
+        vh.AddUIVertexTriangleStream(stream);
+    }
+    #endregion
+}
---
+++ Assets/Script/planet_behavior.cs
@@ -0,0 +1,55 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class planet_behavior : MonoBehaviour {
+    public GameObject target_trail;
+    [Range(0.5f, 1.5f)]
+    public float my_scale;
+    [Range(0.5f,5f)]
+    public float self_spin_spd;
+    [Range(0.2f, 1f)]
+    public float public_spin_spd;
+    public enum planet_type {class_model, real_planet};
+    public planet_type my_type;
+
+    // put myself on trail
+    // moving on the trail
+    // self-spin
+    protected planet_trail _current_trail;
+    protected float _xradius;
+    protected float _yradius;
+    protected Vector3 _center;
+    protected Quaternion _trail_rotation;
+    protected float _start_time;
+
+
+
+    protected bool self_init(GameObject target) {
+        _current_trail = target.GetComponent<planet_trail>();
+        _center = target.transform.position;
+        _trail_rotation = target.transform.rotation;
+        _xradius = _current_trail.XRadius;
+        _yradius = _current_trail.YRadius;
+        //if it has parent, scale it
+        transform.position = _trail_rotation * (_yradius * Vector3.up) + _center;
+        return true;
+    }
+
+    protected void trail_rotate(float xradius, float yradius, float spd, Vector3 center) {
+        Vector3 pos_vector =new Vector3(xradius * Mathf.Sin(spd * Time.time), yradius * Mathf.Cos(spd * Time.time), 0f);
+        _trail_rotation = target_trail.transform.rotation;
+        pos_vector = _trail_rotation * (pos_vector + center);
+        Transform _parent = target_trail.transform;
+        while (_parent.parent != null)
+        {
+           pos_vector *= _parent.parent.localScale.x;
+            _parent = _parent.parent;
+        }
+        transform.position = center + pos_vector;
+    }
+
+    protected void scale_me() {
+        transform.localScale = my_scale * Vector3.one;
+    }
+}
+
