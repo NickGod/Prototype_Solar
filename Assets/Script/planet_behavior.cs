cd8786f3b89e8082f43236b5f3e4e771e2fa2b75
lunaticAnn <lunatic_ann@hotmail.com>
2017-02-10 17:59:45 -0500

    _(:зゝ∠)_

--- Assets/Script/planet_behavior.cs
+++ Assets/Script/planet_behavior.cs
@@ -11,6 +11,7 @@ public class planet_behavior : MonoBehaviour {
     public float public_spin_spd;
     public enum planet_type {class_model, real_planet};
     public planet_type my_type;
+    public bool highlighted;
 
     // put myself on trail
     // moving on the trail
@@ -39,10 +40,12 @@ public class planet_behavior : MonoBehaviour {
         return true;
     }
 
+    float mul_vector = 0.02f;
     protected void trail_rotate(float xradius, float yradius, float spd) {
         _trail_rotation = target_trail.transform.rotation;
         Vector3 pos_vector =new Vector3(xradius * Mathf.Sin(spd * Time.time), yradius * Mathf.Cos(spd * Time.time), 0f);
         Transform _parent = target_trail.transform;
+        //multiply parents' scale
         while (_parent.parent != null)
         {
             pos_vector *= _parent.parent.localScale.x;
@@ -50,20 +53,18 @@ public class planet_behavior : MonoBehaviour {
         }
         pos_vector = _trail_rotation * pos_vector;
         _center = target_trail.transform.position;
-        transform.position =  _center +pos_vector;
+
+        transform.position += mul_vector *(_center +pos_vector -transform.position);
 
     }
-
-    //small_model_rotate will directly put the model on to the line_renderer of the track
-    //every call rotator+1
-    
-    protected void small_model_rotate() {
-          //  int max_index = _that_renderer.
-    }
-
 
     protected void scale_me() {
         transform.localScale = my_scale * Vector3.one;
+    }
+
+    protected void highlight(){
+        Material mat = GetComponent<MeshRenderer>().material; 
+        mat.shader = highlighted?Shader.Find("Custom/RimSelection") : Shader.Find("Standard");
     }
 }
 
